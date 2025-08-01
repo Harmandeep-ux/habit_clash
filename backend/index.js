1efbc97e402e3e4ae2a1f163676d8930d9cd87e1
Harman sahota <sahotaharman59111@gmail.com>
2025-08-01 20:50:24 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ dotenv.config()
 dbConn()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:5173/",
+    credentials:true
+}))
 app.use(cookieParser())
 app.use(express.json())
 
