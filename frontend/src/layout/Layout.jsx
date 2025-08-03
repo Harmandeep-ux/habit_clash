641dee314969b32fce735b4caf49e7b8b0581091
Harman sahota <sahotaharman59111@gmail.com>
2025-08-03 14:01:29 +0530

    layout

---
+++ frontend/src/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Dashboard from '../../pages/Dashboard'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar/>
+    <Dashboard/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
