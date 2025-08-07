ad3522ef526a2b02f3bf692740d1bd4b19ae2f3a
Harman sahota <sahotaharman59111@gmail.com>
2025-08-07 21:36:17 +0530

    route

--- backend/routes/challengeRoute.js
+++ backend/routes/challengeRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createChallenge, getLeaderBoard, getMyChallenges, inviteByUsername, joinChallenge } from '../controllers/challengeController.js'
+import { createChallenge, getAllLeaderboard, getLeaderBoard, getMyChallenges, inviteByUsername, joinChallenge } from '../controllers/challengeController.js'
 import { authMiddleware } from '../middleware/authMiddleware.js'
 import { acceptInvite, rejectInvite } from '../controllers/invitationController.js'
 
@@ -9,7 +9,7 @@ challengeRoute.post('/create',authMiddleware,createChallenge)
 challengeRoute.post('/join/:id',authMiddleware,joinChallenge)
 challengeRoute.get('/mine',authMiddleware,getMyChallenges)
 challengeRoute.get('/leaderboard/:id',authMiddleware,getLeaderBoard)
-challengeRoute.get('/leaderboard',authMiddleware,getLeaderBoard)
+challengeRoute.get('/leaderboard',authMiddleware,getAllLeaderboard)
 challengeRoute.post('/invite/:challengeid',authMiddleware,inviteByUsername)
 challengeRoute.post('/accept/:challengeid',authMiddleware,acceptInvite)
 challengeRoute.post('/reject/:challengeid',authMiddleware,rejectInvite)
